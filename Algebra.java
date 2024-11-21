823ee0695f20bd8cbd84b75de8c23936ad679ecc
Leo Pariente <leopariente@gmail.com>
2024-11-21 10:37:55 +0200

    Fix

--- Algebra.java
+++ Algebra.java
@@ -41,8 +41,14 @@ public class Algebra {
 	// Returns x1 - x2
 	public static int minus(int x1, int x2) {
 		int difference = x1;
-		for (int i = 0; i < x2; i++) {
-			difference--;
+		if (x2 > 0) {
+			for (int i = 0; i < x2; i++) {
+				difference--;
+			}
+		} else {
+			for (int i = 0; i > x2; i--) {
+				difference++;
+			}
 		}
 		return difference;
 	}
@@ -50,8 +56,22 @@ public class Algebra {
 	// Returns x1 * x2
 	public static int times(int x1, int x2) {
 		int result = 0;
-		for (int i = 0; i < x2; i++) {
-			result = plus(result, x1);
+		if (x1 > 0 && x2 > 0) {
+			if (x1 > 0 && x2 > 0) {
+				for (int i = 0; i < x2; i++) {
+					result = plus(result, x1);
+				}
+			}
+		} else if (x1 < 0 && x2 < 0) {
+			for (int i = 0; i > x2; i--) {
+				result = minus(result, x1);
+			}
+		} else {
+			int counter = x2 < x1 ? x2 : x1;
+			int numberToMultiply = counter == x2 ? x1 : x2;
+			for (int i = 0; i > counter; i--) {
+				result = minus(result, numberToMultiply);
+			}
 		}
 		return result;
 	}
@@ -68,27 +88,31 @@ public class Algebra {
 	// Returns the integer part of x1 / x2
 	public static int div(int x1, int x2) {
 		int result = 0;
-		while (times(x2, result) < x1) {
+		int sign = -1;
+		int dividend = Math.abs(x1);
+		int divisor = Math.abs(x2);
+		while (dividend >= Math.abs(divisor)) {
+			dividend = minus(dividend, divisor);
 			result++;
+			if ((x1 > 0 && x2 > 0) || (x1 < 0 && x2 < 0))
+				sign = 1;
 		}
-		return result;
+		return times(result, sign);
 	}
 
 	// Returns x1 % x2
 	public static int mod(int x1, int x2) {
-		int counter = 0;
 		int division = div(x1, x2);
-		while (counter < x1) {
-			counter = plus(counter, division);
-		}
-		return x1 > counter ? x1 - counter : counter - x1;
+		int res = times(division, x2);
+		return x1 - res;
 	}
 
 	// Returns the integer part of sqrt(x)
 	public static int sqrt(int x) {
+		if (x == 0) return 0;
 		int result = 1;
 		while (pow(result, 2) < x) {
-			if (pow(result, 2) > x)
+			if (pow(plus(result, 1), 2) > x)
 				return result;
 			result = plus(result, 1);
 		}
