8f537568b113e1aa2df2e7f0d6164f7b2fac1a0c
Leo Pariente <leopariente@gmail.com>
2024-11-21 10:57:22 +0200

    Fixed errors

--- Anagram.java
+++ Anagram.java
@@ -2,18 +2,18 @@
 public class Anagram {
 	public static void main(String args[]) {
 		// Tests the isAnagram function.
-		System.out.println(isAnagram("silent","listen"));  // true
-		System.out.println(isAnagram("William Shakespeare","I am a weakish speller")); // true
-		System.out.println(isAnagram("Madam Curie","Radium came")); // true
-		System.out.println(isAnagram("Tom Marvolo Riddle","I am Lord Voldemort")); // true
+		System.out.println(isAnagram("silent", "listen")); // true
+		System.out.println(isAnagram("William Shakespeare", "I am a weakish speller")); // true
+		System.out.println(isAnagram("Madam Curie", "Radium came")); // true
+		System.out.println(isAnagram("Tom Marvolo Riddle", "I am Lord Voldemort")); // true
 
 		// Tests the preProcess function.
 		System.out.println(preProcess("What? No way!!!"));
-		
+
 		// Tests the randomAnagram function.
 		System.out.println("silent and " + randomAnagram("silent") + " are anagrams.");
-		
-		// Performs a stress test of randomAnagram 
+
+		// Performs a stress test of randomAnagram
 		String str = "1234567";
 		Boolean pass = true;
 		//// 10 can be changed to much larger values, like 1000
@@ -21,10 +21,11 @@ public class Anagram {
 			String randomAnagram = randomAnagram(str);
 			System.out.println(randomAnagram);
 			pass = pass && isAnagram(str, randomAnagram);
-			if (!pass) break;
+			if (!pass)
+				break;
 		}
 		System.out.println(pass ? "test passed" : "test Failed");
-	}  
+	}
 
 	// Returns true if the two given strings are anagrams, false otherwise.
 	public static boolean isAnagram(String str1, String str2) {
@@ -35,12 +36,13 @@ public class Anagram {
 			if (processedstr2.indexOf(processedstr1.charAt(counter)) == -1)
 				return false;
 			else {
+				counter++;
 				processedstr1 = processedstr1.substring(counter);
 			}
 		}
 		return true;
 	}
-	   
+
 	// Returns a preprocessed version of the given string: all the letter characters
 	// are converted
 	// to lower-case, and all the other characters are deleted, except for spaces,
@@ -48,15 +50,16 @@ public class Anagram {
 	// as is. For example, the string "What? No way!" becomes "whatnoway"
 	public static String preProcess(String str) {
 		String letters = "qwertyuiopasdfghjklzxcvbnm ";
+		String lowerCaseString = str.toLowerCase();
 		String processedString = "";
 		for (int i = 0; i < str.length(); i++) {
-			if (letters.indexOf(str.toLowerCase()) > -1) {
-				processedString += str.charAt(i);
+			if (letters.indexOf(lowerCaseString.charAt(i)) > -1) {
+				processedString += lowerCaseString.charAt(i);
 			}
 		}
 		return processedString;
 	}
-	   
+
 	// Returns a random anagram of the given string. The random anagram consists of
 	// the same
 	// characters as the given string, re-arranged in a random order.
